72701add8dde524eedeb3d77f4150aabd4c61150
Verena Zaiser <development@verena-zaiser.de>
2024-09-09 21:09:32 +0200

    init

---
+++ android/app/src/main/kotlin/de/zaiser/looplab/looplab/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.zaiser.looplab.looplab
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:looplab/app/app.dart';
+import 'package:looplab/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
