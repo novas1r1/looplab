5c1f64bbcded308c8127c8bdd961c95f80e629b1
Verena Zaiser <development@verena-zaiser.de>
2025-02-01 12:54:11 +0100

    updated landing page

--- landingpage/.astro/content.d.ts
+++ landingpage/.astro/content.d.ts
@@ -151,5 +151,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = typeof import("../src/content.config.mjs");
+	export type ContentConfig = typeof import("./../src/content.config.mjs");
 }
