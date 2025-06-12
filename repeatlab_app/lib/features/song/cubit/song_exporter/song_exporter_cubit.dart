31819c74e3314a92b065120fab036667682e86ff
Verena Zaiser <development@verena-zaiser.de>
2025-06-12 16:31:11 +0200

    removed ffmpeg for now

--- repeatlab_app/lib/features/song/cubit/song_exporter/song_exporter_cubit.dart
+++ repeatlab_app/lib/features/song/cubit/song_exporter/song_exporter_cubit.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:dart_mappable/dart_mappable.dart';
-import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
+// import 'package:ffmpeg_kit_flutter_new/ffmpeg_kit.dart';
 import 'package:repeatlab/data/repositories/crash_reporting_repository.dart';
 
 part 'song_exporter_cubit.mapper.dart';
@@ -25,9 +25,8 @@ class SongExporterCubit extends Cubit<SongExporterState> {
       final startSeconds = startTime.inSeconds;
       final durationSeconds = duration.inSeconds;
 
-      final command = '-i "$inputPath" -ss $startSeconds -t $durationSeconds -c copy "$outputPath"';
-
-      await FFmpegKit.execute(command);
+      // final command = '-i "$inputPath" -ss $startSeconds -t $durationSeconds -c copy "$outputPath"';
+      // await FFmpegKit.execute(command);
     } catch (ex, stackTrace) {
       unawaited(crashReportingRepository.reportError(ex, stackTrace));
       emit(
