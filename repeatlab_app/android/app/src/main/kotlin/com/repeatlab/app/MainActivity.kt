7de90e4fe7e14ec7cbda299f47897a85a6ac819d
Verena Zaiser <development@verena-zaiser.de>
2026-02-21 13:33:00 +0100

    updated

--- repeatlab_app/android/app/src/main/kotlin/com/repeatlab/app/MainActivity.kt
+++ repeatlab_app/android/app/src/main/kotlin/com/repeatlab/app/MainActivity.kt
@@ -1,10 +1,5 @@
 package com.repeatlab.app
 
-/*import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity()*/
-
 import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterFragmentActivity() {
-}
+class MainActivity: FlutterFragmentActivity()
