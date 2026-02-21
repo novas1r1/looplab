27ec9ba0c60bc3f8a058d58d472c69e92420a7ea
Verena Zaiser <development@verena-zaiser.de>
2026-02-21 16:27:59 +0100

    fixed missing main activity fragment

--- repeatlab_app/android/app/src/main/kotlin/com/repeatlab/app/MainActivity.kt
+++ repeatlab_app/android/app/src/main/kotlin/com/repeatlab/app/MainActivity.kt
@@ -1,5 +1,5 @@
 package com.repeatlab.app
 
-import io.flutter.embedding.android.FlutterFragmentActivity
+import com.ryanheise.audioservice.AudioServiceFragmentActivity
 
-class MainActivity: FlutterFragmentActivity()
+class MainActivity: AudioServiceFragmentActivity()
